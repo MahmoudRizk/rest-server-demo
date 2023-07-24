2c63ef6106b594a21b9454fd200722fbb6615cd7
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2023-07-25 00:40:11 +0300

    feat: add get students endpoint.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('/students', function (Request $request) {   
+    return DB::select(DB::raw("select name, email from students"));
 });
